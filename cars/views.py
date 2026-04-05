c59984f65c79fd4b3aeb929caedb4ae994a1da6c
Caio Sampaio <cmsampaio71@gmail.com>
2026-04-05 19:12:14 -0300

    finalizando o cadastro de carros

--- cars/views.py
+++ cars/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
+from cars.forms import CarForm
 
 def cars_view(request):
-    cars_1 = Car.objects.all().order_by('-model') # ou -model = faz pela ordem contrária
+    cars = Car.objects.all().order_by('-model') # ou -model = faz pela ordem contrária
     search = request.GET.get('search')
     # tratando a varíavel
     if search:
-        cars_1 = cars_1.filter(model__icontains=search) # busca por qualquer elemento com esse contain ignorando capitalização e ordenando por modelo
+        cars = cars.filter(model__icontains=search) # busca por qualquer elemento com esse contain ignorando capitalização e ordenando por modelo
     
     #cars = Car.objects.all() # busca todos os objetos / registros
     #cars = Car.objects.filter(brand__name=search) filtrando e recebe o ID e não a string e usando __name é possível filtrar pelo nome
     # por model
         
     return render(request, 'cars.html',
-                  {'cars': cars_1},)
+                  {'cars': cars},)
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarForm()
+    return render(request, 'new_car.html', {'new_car_form': new_car_form})
