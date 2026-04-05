891374a4a94f94fce9a1c2ed76ea55f06513f300
Caio Sampaio <cmsampaio71@gmail.com>
2026-04-05 17:18:37 -0300

    iniciando o template e corrigindo bug de fotos

--- cars/views.py
+++ cars/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars_1 = Car.objects.all()
+    cars_1 = Car.objects.all().order_by('-model') # ou -model = faz pela ordem contrária
     search = request.GET.get('search')
     # tratando a varíavel
     if search:
-        cars_1 = Car.objects.filter(model__contains=search) # busca por qualquer elemento com esse contain
+        cars_1 = cars_1.filter(model__icontains=search) # busca por qualquer elemento com esse contain ignorando capitalização e ordenando por modelo
     
     #cars = Car.objects.all() # busca todos os objetos / registros
     #cars = Car.objects.filter(brand__name=search) filtrando e recebe o ID e não a string e usando __name é possível filtrar pelo nome
