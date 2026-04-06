18f675ae7b0003f9f233d2c9969772d8ef0b5ccf
Caio Sampaio <cmsampaio71@gmail.com>
2026-04-06 10:34:24 -0300

    refatorando com views basic

--- cars/views.py
+++ cars/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('-model') # ou -model = faz pela ordem contrária
-    search = request.GET.get('search')
-    # tratando a varíavel
-    if search:
-        cars = cars.filter(model__icontains=search) # busca por qualquer elemento com esse contain ignorando capitalização e ordenando por modelo
+class CarView(View):
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')
+        search = request.GET.get('search')
+        # tratando a varíavel
+        if search:
+            cars = cars.filter(model__icontains=search)
+            
+        return render(
+            request, 
+            'cars.html',
+            {'cars': cars},
+        )
+
+class NewCarView(View):
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
     
-    #cars = Car.objects.all() # busca todos os objetos / registros
-    #cars = Car.objects.filter(brand__name=search) filtrando e recebe o ID e não a string e usando __name é possível filtrar pelo nome
-    # por model
-        
-    return render(request, 'cars.html',
-                  {'cars': cars},)
-
-def new_car_view(request):
-    if request.method == 'POST':
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
