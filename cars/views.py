b579ffc70f705434a3cf81150cd933bec11c4437
Caio Sampaio <cmsampaio71@gmail.com>
2026-04-06 10:51:34 -0300

    refatorando CarListView

--- cars/views.py
+++ cars/views.py
@@ -1,21 +1,24 @@
+from typing import Any
+
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView
 
-class CarView(View):
-    def get(self, request):
-        cars = Car.objects.all().order_by('model')
-        search = request.GET.get('search')
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model') # filtrando como cars = Car.objects.all().order_by('model') porém usando Heranças
+        search = self.request.GET.get('search')
         # tratando a varíavel
         if search:
             cars = cars.filter(model__icontains=search)
-            
-        return render(
-            request, 
-            'cars.html',
-            {'cars': cars},
-        )
+        return cars
 
 class NewCarView(View):
     def get(self, request):
