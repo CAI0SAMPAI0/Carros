67ab6e1cec1e7a2fa6ec0129a5441389510152ca
Caio Sampaio <cmsampaio71@gmail.com>
2026-04-07 09:33:17 -0300

    fix: login e register

--- accounts/views.py
+++ accounts/views.py
@@ -5,27 +5,20 @@ from django.contrib.auth import authenticate, login, logout
 def register_view(request):
     user_form = UserCreationForm()
     if request.method == 'POST':
-        user_form = user_form
+        user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
             return redirect('login')
-    else:
-        user_form = user_form
     return render(request, 'register.html', {"user_form": user_form})
 
 def login_view(request):
     login_form = AuthenticationForm()
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        login_form = AuthenticationForm(data=request.POST)
+        if login_form.is_valid():
+            user = login_form.get_user()
             login(request, user)
             return redirect('cars_list')
-        else:
-            login_form = login_form
-    else:
-        login_form = login_form
     return render(request, 'login.html', {'login_form': login_form})
 
 def logout_view(request):
